a41349f27cde7c7f1bbf84d324bf61ddb21925c0
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-02 14:28:51 +0530

    Guessing Game

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
 from Function import guess_the_number
 
-guess_the_number()
+gtn()
--- function.py
+++ function.py
@@ -1,25 +1,22 @@
 import random
 
 
-def guess_the_number():
-    secret_number = random.randint(1, 1000)
+def gtn():
+    sec_num = random.randint(1, 1000)
 
-    attempts = 0
+    no_of_tries = 0
 
     print("Welcome to Guess The Number Game!")
     print("I'm thinking of a number between 1 and 1000. Can you guess it?")
 
     while True:
         user_guess = int(input("Enter your guess: "))
-        attempts += 1
+        no_of_tries += 1
 
-        if user_guess < secret_number:
+        if user_guess < sec_num:
             print("Too low! Try again.")
-        elif user_guess > secret_number:
+        elif user_guess > sec_num:
             print("Too high! Try again.")
         else:
-            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            print("Congratulations! You guessed the number in", no_of_tries, "tries.")
             break
-
-
-guess_the_number()
